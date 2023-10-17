ee500968fd2191e21b28c2943c25b08726322d74
AyanamiSoryu <NikolskiyClarinet@gmail.com>
2023-10-17 19:41:13 +0400

    Added TourFirstPage component at the top of the site

--- src/ui/components/TourFirstPart/TourFirstPart.tsx
+++ src/ui/components/TourFirstPart/TourFirstPart.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import classNames from './TourFirstPart.modules.scss';
 import { useTranslation } from "react-i18next";
+import jpeg from '../../../data/imageMock/tourfirstimage.jpg'
 
 const TourFirstPart = () => {
 
     const { t } = useTranslation();
     return (
-        <div className={classNames.root} style={{backgroundImage: 'url(https://cdn.tripster.ru/thumbs2/0e7c6bc8-faf2-11ed-82ad-12199f507f99.1600x900.jpeg)'}}>
+        <div className={classNames.root} style={{backgroundImage: `url(${jpeg})`}}>
             <div className={classNames.margin}>
                 <h1 className={classNames.h1}>{t('Кахетия — родина вина, чачи и красоты!')}</h1>
                 <div className={classNames.degustation}>{t('Дегустации, очаровательный Сигнахи, монастырь Бодбе и Телави в экскурсии на микроавтобусе')}</div>
